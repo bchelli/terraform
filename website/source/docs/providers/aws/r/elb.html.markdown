opexsw/terraform-4-aws-provider

Add docs and warning
Add web documentation, and include a warning about the conflict between
aws_elb_attachment and aws_elb.instances.

website/source/docs/providers/aws/r/elb.html.markdown
@@ -10,6 +10,12 @@ description: |-
 
 Provides an Elastic Load Balancer resource.
 
+~> **NOTE on ELB Instances and ELB Attachments:** Terraform currently
+provides both a standalone [ELB Attachment resource](elb_attachment.html)
+(describing the instances attached to an ELB), and an ELB resource with
+`instances` defined in-line. At this time you cannot use an ELB with in-line
+instaces in conjunction with an ELB Attachment resource. Doing so will cause a
+conflict of rule settings and will overwrite rules.
 ## Example Usage
 
 ```

bchelli/terraform

website/source/docs/providers/aws/r/elb.html.markdown
@@ -10,6 +10,12 @@ description: |-
 
 Provides an Elastic Load Balancer resource.
 
+~> **NOTE on ELB Instances and ELB Attachments:** Terraform currently
+provides both a standalone [ELB Attachment resource](elb_attachment.html)
+(describing an instance attached to an ELB), and an ELB resource with
+`instances` defined in-line. At this time you cannot use an ELB with in-line
+instaces in conjunction with a ELB Attachment resources. Doing so will cause a
+conflict of rule settings and will overwrite rules.
 ## Example Usage
 
 ```